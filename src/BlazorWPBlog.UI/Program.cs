5c84071b8f73ce3a83d816ffbbe2f2848a4cc956
Davide Guida <Davide.Guida@equisoft.com>
2020-11-17 21:19:56 -0500

    first commit

---
+++ src/BlazorWPBlog.UI/Program.cs
@@ -0,0 +1,29 @@
+using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Net.Http;
+using System.Threading.Tasks;
+using BlazorWPBlog.UI.Services;
+using WordPressPCL;
+
+namespace BlazorWPBlog.UI
+{
+    public class Program
+    {
+        public static async Task Main(string[] args)
+        {
+            var builder = WebAssemblyHostBuilder.CreateDefault(args);
+            builder.RootComponents.Add<App>("app");
+
+            builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
+
+            var wpApiEndpoint = builder.Configuration["WP_Endpoint"];
+            var client = new WordPressClient(wpApiEndpoint);
+            builder.Services.AddSingleton(client);
+
+            builder.Services.AddSingleton<ITagService, TagService>();
+
+            await builder.Build().RunAsync();
+        }
+    }
+}
---
+++ src/BlazorWPBlog.UI/Services/TagService.cs
@@ -0,0 +1,62 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+using Microsoft.Extensions.Logging;
+using WordPressPCL;
+
+namespace BlazorWPBlog.UI.Services
+{
+    public interface ITagService
+    {
+        Task<WordPressPCL.Models.Tag> GetTagAsync(int id);
+    }
+
+    public class TagService : ITagService
+    {
+        private readonly WordPressClient _client;
+        private readonly ILogger<TagService> _logger;
+        private static readonly SemaphoreSlim _semaphore;
+        private static IDictionary<int, WordPressPCL.Models.Tag> _tags;
+
+        static TagService()
+        {
+            _semaphore = new SemaphoreSlim(1, 1);
+        }
+
+        public TagService(WordPressClient client, ILogger<TagService> logger)
+        {
+            _client = client;
+            _logger = logger;
+        }
+
+        public async Task<WordPressPCL.Models.Tag> GetTagAsync(int id)
+        {
+            await EnsureTagsAsync(_client);
+
+            _tags.TryGetValue(id, out var tag);
+            return tag;
+        }
+
+        private async Task EnsureTagsAsync(WordPressClient client)
+        {
+            if (null != _tags)
+                return;
+
+            await _semaphore.WaitAsync();
+            try
+            {
+                if (null == _tags)
+                {
+                    _logger.LogInformation($"fetching all tags...");
+                    var tags = await client.Tags.GetAll();
+                    _tags = tags?.ToDictionary(t => t.Id);
+                }
+            }
+            finally
+            {
+                _semaphore.Release();
+            }
+        }
+    }
+}
